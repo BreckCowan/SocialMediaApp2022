39c71087f5fd5de6422633121b1d4015c546f21d
Breck Cowan <breckcowan@gmail.com>
2022-06-22 20:17:02 -0500

    added _app.js and imported bootstrap for styling

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9190ef8ba4e48bd6"; }
+/******/ 		__webpack_require__.h = function() { return "fa70ba8168ee0997"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
