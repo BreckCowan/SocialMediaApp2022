fae79d434512aa42c1927aa1d2c8c39a7ec60634
Breck Cowan <breckcowan@gmail.com>
2022-06-23 21:44:27 -0500

    stylized nav

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bd33944008441cdc"; }
+/******/ 		__webpack_require__.h = function() { return "052f0b7ac72d83bc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/components/Nav.js
+++ client/components/Nav.js
@@ -2,23 +2,23 @@ import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="nav justify-content-center">
+    <nav className="nav justify-content-around bg-dark d-flex">
       {/* Link to home page */}
       <li className="nav-item">
-        <Link href="/" className="nav-link active">
-          <a>Home</a>
+        <Link href="/">
+          <a className="nav-link text-light">Home</a>
         </Link>
       </li>
       {/* Link to login page */}
       <li className="nav-item">
-        <Link href="login" className="nav-link">
-          <a>Login</a>
+        <Link href="/login">
+          <a className="nav-link text-light">Login</a>
         </Link>
       </li>
       {/* Link to register page */}
       <li className="nav-item">
-        <Link href="register" className="nav-link">
-          <a>Register</a>
+        <Link href="/register">
+          <a className="nav-link text-light">Register</a>
         </Link>
       </li>
     </nav>
