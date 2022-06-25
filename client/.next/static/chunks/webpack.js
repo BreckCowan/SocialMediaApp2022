b3d1f912423529316a0287e61baf4ac97d10053e
Breck Cowan <breckcowan@gmail.com>
2022-06-25 13:26:24 -0500

    added server, setup .env, server is running and db is connected

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d0a4934bef1337e3"; }
+/******/ 		__webpack_require__.h = function() { return "8c20c9b614d24dd1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
