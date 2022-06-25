c49ec4d034a2a23184c2af3cc9a62f4e5f8eeafa
Breck Cowan <breckcowan@gmail.com>
2022-06-25 15:18:11 -0500

    connected helper to controller to add hashed passwords

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,30 @@
-export const register = (req, res) => {
-  console.log("Register Endpoint => ", req.body);
+import User from "../models/user";
+import { hashPassword, comparePassword } from "../helpers/auth";
+
+export const register = async (req, res) => {
+  //   console.log("Register Endpoint => ", req.body);
+  const { name, email, password, secret } = req.body;
+  //validation
+  if (!name) return res.status(400).send("Name is required");
+  if (!password || password.length < 6)
+    return res
+      .status(400)
+      .send("Password is required and must be at least 6 characters long");
+  if (!secret) return res.status(400).send("Answer is required");
+  const exist = await User.findOne({ email });
+  if (exist) return res.status(400).send("Email is already taken");
+  //hash password
+  const hashedPassword = await hashPassword(password);
+
+  const user = new User({ name, email, password: hashedPassword, secret });
+  try {
+    await user.save();
+    // console.log('Registered User => ', user)
+    return res.json({
+      ok: true,
+    });
+  } catch (err) {
+    console.log("Register Failed => ", err);
+    return res.status(400).send("Error, please try again");
+  }
 };
--- server/models/user.js
+++ server/models/user.js
@@ -1,34 +1,35 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 64,
+    },
+    secret: {
+      type: String,
+      required: true,
+    },
+    about: {},
+    photo: String,
+    following: [{ type: Schema.ObjectId, ref: "User" }],
+    followers: [{ type: Schema.ObjectId, ref: "User" }],
   },
-  email: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    trim: true,
-    min: 6,
-    max: 64,
-  },
-  secret: {
-    type: String,
-    required: true,
-  },
-  about: {},
-  photo: String, 
-  following: [{type: Schema.ObjectID, ref: 'User'}],
-  followers: [{type: Schema.ObjectID, ref: 'User'}],
-}, 
-{timestamps: true}
+  { timestamps: true }
 );
 
-export default mongoose.model('User', userSchema);
+export default mongoose.model("User", userSchema);
