0d095903176a63f65b6858eba65db53edfd805f0
Breck Cowan <breckcowan@gmail.com>
2022-06-25 13:43:19 -0500

    automated routes and added register route path to controllers

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import { readdirSync } from "fs";
 
 const morgan = require("morgan");
 require("dotenv").config();
@@ -10,8 +11,8 @@ const app = express();
 //database
 mongoose
   .connect(process.env.DATABASE, {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => console.log("Database is Connected"))
   .catch((err) => console.log("Database Connection Error => ", err));
@@ -21,9 +22,8 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: ["http://localhost:3000"] }));
 
-app.post("/api/register", (req, res) => {
-  console.log("Register Endpoint => ", req.body);
-});
+//automating loading of routes
+readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)))
 
 const port = process.env.PORT || 8000;
 
