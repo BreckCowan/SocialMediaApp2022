582e908cf5b09ac4ed601faf691cd50e8aac0530
Breck Cowan <breckcowan@gmail.com>
2022-06-25 12:17:28 -0500

    moved style sheet out of head and imported directly into _app.js

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ef490c6a80e4c900"; }
+/******/ 		__webpack_require__.h = function() { return "befcce0f46150ff5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,3 +1,4 @@
+import '../public/css/styles.css'
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "../components/Nav";
 import Head from "next/head";
@@ -5,9 +6,7 @@ import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <link rel="stylesheet" href="/css/styles.css" />
-      </Head>
+      <Head />
       <Nav />
       <Component {...pageProps} />
     </>
