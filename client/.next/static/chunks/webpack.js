75b5e3d52fc12d2584544a1b9ddc35e716ba2c7d
Breck Cowan <breckcowan@gmail.com>
2022-06-25 20:22:37 -0500

    manually installed toast and ant design into _app.js

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "51acade915c90603"; }
+/******/ 		__webpack_require__.h = function() { return "56c16cc01af3fc28"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,13 +1,27 @@
-import '../public/css/styles.css'
+import "../public/css/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "../components/Nav";
 import Head from "next/head";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head />
       <Nav />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Component {...pageProps} />
     </>
   );
