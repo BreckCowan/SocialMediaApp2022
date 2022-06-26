d3fb3b3f2e257da510a759dfc52d10157b08c1a3
Breck Cowan <breckcowan@gmail.com>
2022-06-26 10:54:20 -0500

    added bg image to register page

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9c37793eae0bc31f"; }
+/******/ 		__webpack_require__.h = function() { return "b0d2787bdfda41d6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
